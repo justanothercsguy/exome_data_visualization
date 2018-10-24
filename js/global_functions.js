4a02c52bf0aac6d4a654a454a50005553e6b82c8
justanothercsguy <salsazishi@gmail.com>
2018-10-24 17:11:00 -0400

    Created function that updates zoomed in chart based on yAxisVariable updates
    Function redraws y axis scale, y axis label, and variant heights based on the updated y axis variable. Connecting this function to the buttons will be difficult though, and it needs to return handle to lollipop circles so that upon mouse hovering on the lollipop circles, the variant data appears.

--- js/global_functions.js
+++ js/global_functions.js
@@ -1,15 +1,34 @@
 // change y axis variable, which will subsequently cause the whole webpage to reload
 function changeYAxisVariable(yAxisVariable) {
-    if (yAxisVariable == 'MAF') {
-        console.log('MAF');
-    }
-    if (yAxisVariable == 'alleleNumber') {
-        console.log('alleleNumber');
-    }
 
-    var chartZoomedOut = document.getElementsByClassName("chart-zoomed-out")[0];
-    var chartZoomedIn = document.getElementsByClassName("chart-zoomed-in")[0];
-    console.log(chartZoomedOut);
-    console.log(chartZoomedIn);
-    console.log(variant_map);
+  if (yAxisVariable == 'MAF') {
+      console.log('MAF');
+  }
+  if (yAxisVariable == 'alleleNumber') {
+      console.log('alleleNumber');
+  }
+
+  var chartZoomedOut = document.getElementsByClassName("chart-zoomed-out")[0];
+  var chartZoomedIn = document.getElementsByClassName("chart-zoomed-in")[0];
+  console.log(chartZoomedOut);
+  console.log(chartZoomedIn);
+  console.log(variant_map);
+
+}
+
+function updateZoomedInChartToYAxisVariable(variantTransform, yAxisVariableString, 
+  variantTransform, data, scaleSingleExonToChart, CLICKED_EXON_OFFSET, 
+  BASE_PAIRS_OUTSIDE_EXON_LIMIT, lollipop, yScaleLollipop, variant_map_single_exon, 
+  yAxisTransform, height, margin) {
+  
+  // clear the transform of all g objects
+  variantTransform.selectAll("g").remove()
+
+  var lollipopCircle = addLollipopToZoomedInChart(yAxisVariableString, variantTransform, 
+    data, scaleSingleExonToChart, CLICKED_EXON_OFFSET, BASE_PAIRS_OUTSIDE_EXON_LIMIT, 
+    lollipop, yScaleLollipop, variant_map_single_exon);
+
+  drawYAxisScaleAndLabel(yAxisTransform, height, margin, yScaleLollipop);
+  
+  return lollipopCircle;
 }
