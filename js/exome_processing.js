8ef15558dc8bd08aff88a2f35d06f54d408d311d
justanothercsguy <salsazishi@gmail.com>
2018-10-23 21:31:55 -0400

    Moved more functions from exome_processing.js to tools_zoomed_out_chart.js

--- js/exome_processing.js
+++ js/exome_processing.js
@@ -350,30 +350,6 @@ function getRange(gene_uniform_intron_length, splitIntronIndices, INTRON_LENGTH)
   return range;
 }
 
-
-// add rectangle represneting exons to a svg transform handle
-function addRectToChart(chartTransform, exonLengths, scaleUniformIntronsToChart,
-  exonStartsWithUniformIntronLengths, exonBarColor, barHeightCoding) {
-
-  chartTransform.selectAll("g").remove();
-  var bar = chartTransform.selectAll("g")
-    .data(exonLengths)
-    .enter().append("g")
-    .attr("transform", function (d, i) {
-      return "translate(" + scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i])
-        + "," + 0 + ")";
-    });
-
-  bar.append("rect")
-    .attr("fill", exonBarColor)
-    .attr("width", scaleUniformIntronsToChart)
-    .attr("height", function (d, i) {
-      return barHeightCoding;
-    });
-
-  return bar;
-}
-
 // add white rectangles to area that occur before cdsStart and area that occurs after cdsEnd
 function addNonCodingRectToZoomedOutChart(chartTransform, nonCodingExonLengths, barHeightCoding,
   barHeightNonCoding, nonCodingXPositionStarts, scaleUniformIntronsToChart) {
@@ -428,13 +404,6 @@ function getYAxisScaleFromVariable(variable, height, data) {
       .range([ 0, height ]);
   }
 
-  // var yScaleMin = d3.min(data, function(d) { return d.alleleFrequency; });
-  // var yScaleMinExponent = Math.floor( Math.log(yScaleMin) / Math.log(10) );
-  // yScaleMin = Math.pow(10, yScaleMinExponent);
-
-  // var yScaleLollipop = d3.scaleLog()
-  //     .domain([ yScaleMin, 1 ])
-  //     .range([ 0, height ]);
   return yScaleLollipop;
 }
 
@@ -482,46 +451,5 @@ function getVariantInformationForTooltip(variant) {
     + "alleleFrequency: " + variant_map[d.position][0].alleleFrequency + "<br/>"
     + "number of variants: " + variant_map[d.position].length
   )
-}
-
-function getIntronStartsAndEndsWithUniformIntronLength(scaleUniformIntronsToChart,
-  exonStartsWithUniformIntronLengths, exonEndsWithUniformIntronLengths, NUM_EXONS) {
-  var intronStartsAndEndsWithUniformIntronLength = [];
-
-  // originally from i = 1 to NUM_EXONS, but I removed nonCoding partitions so i = 0 now
-  for (var i = 0; i < NUM_EXONS - 1; i++) {
-    var start1 = [scaleUniformIntronsToChart(exonEndsWithUniformIntronLengths[i]), barHeightNonCoding];
-    var end1 = [scaleUniformIntronsToChart(exonEndsWithUniformIntronLengths[i] + INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
-    var start2 = [scaleUniformIntronsToChart(exonEndsWithUniformIntronLengths[i] + INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
-    var end2 = [scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i + 1] - INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
-    var start3 = [scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i + 1] - INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
-    var end3 = [scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i + 1]), barHeightNonCoding];
-    intronStartsAndEndsWithUniformIntronLength.push([start1, end1]);
-    intronStartsAndEndsWithUniformIntronLength.push([start2, end2]);
-    intronStartsAndEndsWithUniformIntronLength.push([start3, end3]);
-  }
-
-  return intronStartsAndEndsWithUniformIntronLength;
-}
-
-function addIntronLinesToSvgHandle(chartTransform, intronStartsAndEndsWithUniformIntronLength) {
-  var lines = chartTransform.selectAll("path")
-    .data(intronStartsAndEndsWithUniformIntronLength)
-    .enter().append("path")
-    .style("stroke", function (d, i) {
-      if (i % 3 != 1) { return "black"; }
-      return "grey";
-    })
-    .attr("stroke-width", function (d, i) {
-      if (i % 3 != 1) { return 4; }
-      return 2;
-    })
-    .style("stroke-dasharray", function (d, i) {
-      if (i % 3 != 1) { return "2,2"; }
-      return "2,1";
-    })
-    .attr("d", d3.line());
-
-  return lines;
 }
 
--- js/tools_zoomed_out_chart.js
+++ js/tools_zoomed_out_chart.js
@@ -1,51 +1,119 @@
-function addLollipopToZoomedOutChart(yAxisVariableString, variantTransform, data, lollipop, OFFSET,
-    scaleUniformIntronsToChart, scaleVariableIntronsToUniformIntrons, yScaleLollipop) {
-    
-    variantTransform.selectAll("g").remove();
-  
-    var lollipopRect = variantTransform.selectAll("g")
-        .data(data)
-      .enter().append("g")
-        .attr("transform", function(d) {
-          return "translate(" + 
-            scaleUniformIntronsToChart(
-              scaleVariableIntronsToUniformIntrons(d.position - OFFSET)
-            ) + "," + 0 + ")";
-        })
-        .append("rect")
-        .attr("width", lollipop.width)
-        .attr("height", function(d) {
-          if (yAxisVariableString == 'MAF') {
-            return yScaleLollipop(variant_map[d.position][0].alleleFrequency);
-          }
-          else if (yAxisVariableString == 'alleleNumber') {
-            return yScaleLollipop(variant_map[d.position][0].alleleNumber);
-          }
-        })
-        .attr('fill', function(d) {
-          return variantAnnotationToLollipopColor(d.annotation);
-        });
-  
-      var lollipopCircle = variantTransform.selectAll('circle')
-        .data(data)
-        .enter().append('circle')
-        .attr('cx', function (d) {
-          return scaleUniformIntronsToChart(
-            scaleVariableIntronsToUniformIntrons(d.position - OFFSET)
-          );
-        })
-        .attr('cy', function (d) {
-          if (yAxisVariableString == 'MAF') {
-            return yScaleLollipop(variant_map[d.position][0].alleleFrequency);
-          }
-          else if (yAxisVariableString == 'alleleNumber') {
-            return yScaleLollipop(variant_map[d.position][0].alleleNumber);
-          }
-        })
-        .attr('r', lollipop.radius)
-        .attr('fill', function (d) {
-          return variantAnnotationToLollipopColor(d.annotation);
-        });
-      
-      return lollipopCircle;
+// add rectangle representing exons to a svg transform handle
+function addRectToChart(chartTransform, exonLengths, scaleUniformIntronsToChart,
+  exonStartsWithUniformIntronLengths, exonBarColor, barHeightCoding) {
+
+  chartTransform.selectAll("g").remove();
+  var bar = chartTransform.selectAll("g")
+    .data(exonLengths)
+    .enter().append("g")
+    .attr("transform", function (d, i) {
+      return "translate(" + scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i])
+        + "," + 0 + ")";
+    });
+
+  bar.append("rect")
+    .attr("fill", exonBarColor)
+    .attr("width", scaleUniformIntronsToChart)
+    .attr("height", function (d, i) {
+      return barHeightCoding;
+    });
+
+  return bar;
+}
+
+function getIntronStartsAndEndsWithUniformIntronLength(scaleUniformIntronsToChart,
+  exonStartsWithUniformIntronLengths, exonEndsWithUniformIntronLengths, NUM_EXONS) {
+  var intronStartsAndEndsWithUniformIntronLength = [];
+
+  // originally from i = 1 to NUM_EXONS, but I removed nonCoding partitions so i = 0 now
+  for (var i = 0; i < NUM_EXONS - 1; i++) {
+    var start1 = [scaleUniformIntronsToChart(exonEndsWithUniformIntronLengths[i]), barHeightNonCoding];
+    var end1 = [scaleUniformIntronsToChart(exonEndsWithUniformIntronLengths[i] + INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
+    var start2 = [scaleUniformIntronsToChart(exonEndsWithUniformIntronLengths[i] + INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
+    var end2 = [scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i + 1] - INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
+    var start3 = [scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i + 1] - INTRON_LENGTH_SPLIT_INTO_THREE), barHeightNonCoding];
+    var end3 = [scaleUniformIntronsToChart(exonStartsWithUniformIntronLengths[i + 1]), barHeightNonCoding];
+    intronStartsAndEndsWithUniformIntronLength.push([start1, end1]);
+    intronStartsAndEndsWithUniformIntronLength.push([start2, end2]);
+    intronStartsAndEndsWithUniformIntronLength.push([start3, end3]);
   }
+
+  return intronStartsAndEndsWithUniformIntronLength;
+}
+
+// add dashed lines representing intron area between exons
+// the sides of the intron area up to BASE_PAIRS_EXON_LIMIT are darkened to show that 
+// variants are still shown in those areas, while the middle section is lighter to show
+// that variants are not shown in the middle area
+function addIntronLinesToSvgHandle(chartTransform, intronStartsAndEndsWithUniformIntronLength) {
+  var lines = chartTransform.selectAll("path")
+    .data(intronStartsAndEndsWithUniformIntronLength)
+    .enter().append("path")
+    .style("stroke", function (d, i) {
+      if (i % 3 != 1) { return "black"; }
+      return "grey";
+    })
+    .attr("stroke-width", function (d, i) {
+      if (i % 3 != 1) { return 4; }
+      return 2;
+    })
+    .style("stroke-dasharray", function (d, i) {
+      if (i % 3 != 1) { return "2,2"; }
+      return "2,1";
+    })
+    .attr("d", d3.line());
+
+  return lines;
+}
+
+function addLollipopToZoomedOutChart(yAxisVariableString, variantTransform, data, lollipop, OFFSET,
+  scaleUniformIntronsToChart, scaleVariableIntronsToUniformIntrons, yScaleLollipop) {
+
+  variantTransform.selectAll("g").remove();
+
+  var lollipopRect = variantTransform.selectAll("g")
+    .data(data)
+    .enter().append("g")
+    .attr("transform", function (d) {
+      return "translate(" +
+        scaleUniformIntronsToChart(
+          scaleVariableIntronsToUniformIntrons(d.position - OFFSET)
+        ) + "," + 0 + ")";
+    })
+    .append("rect")
+    .attr("width", lollipop.width)
+    .attr("height", function (d) {
+      if (yAxisVariableString == 'MAF') {
+        return yScaleLollipop(variant_map[d.position][0].alleleFrequency);
+      }
+      else if (yAxisVariableString == 'alleleNumber') {
+        return yScaleLollipop(variant_map[d.position][0].alleleNumber);
+      }
+    })
+    .attr('fill', function (d) {
+      return variantAnnotationToLollipopColor(d.annotation);
+    });
+
+  var lollipopCircle = variantTransform.selectAll('circle')
+    .data(data)
+    .enter().append('circle')
+    .attr('cx', function (d) {
+      return scaleUniformIntronsToChart(
+        scaleVariableIntronsToUniformIntrons(d.position - OFFSET)
+      );
+    })
+    .attr('cy', function (d) {
+      if (yAxisVariableString == 'MAF') {
+        return yScaleLollipop(variant_map[d.position][0].alleleFrequency);
+      }
+      else if (yAxisVariableString == 'alleleNumber') {
+        return yScaleLollipop(variant_map[d.position][0].alleleNumber);
+      }
+    })
+    .attr('r', lollipop.radius)
+    .attr('fill', function (d) {
+      return variantAnnotationToLollipopColor(d.annotation);
+    });
+
+  return lollipopCircle;
+}
