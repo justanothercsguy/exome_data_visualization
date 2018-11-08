495ea4cd31809e316c6ee876c965d587e8fa7dfd
justanothercsguy <salsazishi@gmail.com>
2018-11-08 15:52:31 -0500

    Writing your first Django app, part 3
    Added view to /visualization/ url that displays the first 10 gene names from the visualization_gene table.

--- exomesite/visualization/urls.py
+++ exomesite/visualization/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /visualization/
     path('', views.index, name='index'),
+
+    # ex: /visualization/1
+    path('<int:id>/', views.query, name='query'),
 ]
--- exomesite/visualization/views.py
+++ exomesite/visualization/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Gene
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the visualization index.")
+    sample_gene_list = Gene.objects.order_by('-id')[:10]
+    # remove gene objects that have duplicate name2 strings
+    output = ', '.join(list(set([g.name2 for g in sample_gene_list]))) 
+    return HttpResponse(output)
+
+def query(request, id):
+    return HttpResponse("You're querying the gene %s." % id)
