dce6f3107f70fec5b49515b71c58af07029b7286
justanothercsguy <salsazishi@gmail.com>
2018-11-08 16:01:57 -0500

    Writing your first Django app, part 3
    Added a template folder and template/visualization/index.html which displays a list of te first 10 genes from the visualization_gene table. Connected that view to index() in exomesite/visualization/views.py

--- exomesite/visualization/views.py
+++ exomesite/visualization/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader 
 
 from .models import Gene
 
+
 def index(request):
-    sample_gene_list = Gene.objects.order_by('-id')[:10]
-    # remove gene objects that have duplicate name2 strings
-    output = ', '.join(list(set([g.name2 for g in sample_gene_list]))) 
-    return HttpResponse(output)
+    sample_gene_list = list(set(Gene.objects.order_by('-name2')[:10]))
+    template = loader.get_template('visualization/index.html')
+    context = {
+        'sample_gene_list': sample_gene_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def query(request, id):
     return HttpResponse("You're querying the gene %s." % id)
