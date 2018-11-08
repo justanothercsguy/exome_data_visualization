abf2204c3d879c5b0d6bb55a7365cb75f65b11b0
justanothercsguy <salsazishi@gmail.com>
2018-11-07 19:35:33 -0500

    Writing your first Django app, part 2
    Initialized db.sqlite file which contains Django tables for configuration and admin purposes, as well my own data consisting of two tables: exons and variants_SAMD11. The exons table contains six genes and their exon coordinates, but some genes have variations so there are eight rows instead of six rows. The variants_SAMD11 table contains all the variants for the gene SAMD11. I will use this table as the model for the Variant object.

--- exomesite/exomesite/settings.py
+++ exomesite/exomesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'visualization.apps.VisualizationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- exomesite/visualization/models.py
+++ exomesite/visualization/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Gene(models.Model):
+    name = models.CharField(max_length=200)
+    chrom = models.CharField(max_length=200)
+    strand = models.CharField(max_length=200)
+    txstart = models.BigIntegerField()  # Field name made lowercase.
+    txend = models.BigIntegerField()  # Field name made lowercase.
+    cdsstart = models.BigIntegerField()  # Field name made lowercase.
+    cdsend = models.BigIntegerField()  # Field name made lowercase.
+    exoncount = models.IntegerField()  # Field name made lowercase.
+    exonstarts = models.TextField()  # Field name made lowercase.
+    exonends = models.TextField()  # Field name made lowercase.
+    score = models.IntegerField() # value is always 0
+    name2 = models.CharField(max_length=200)
+    cdsstartstat = models.CharField(max_length=200)  # Field name made lowercase.
+    cdsendstat = models.CharField(max_length=200)  # Field name made lowercase.
+    exonframes = models.TextField()  # Field name made lowercase.
+
+    # some genes have variants so use both names for distinguishing variants
+    def __str__(self):
+        return self.name2 + ' (' + self.name + ')'
