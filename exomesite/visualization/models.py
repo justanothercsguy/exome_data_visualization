43180482435197cf9fee282c8b3dfe990e3802fd
justanothercsguy <salsazishi@gmail.com>
2018-11-07 19:55:15 -0500

    Writing your first Django app, part 2
    Added the Variants model to exomesite/visualizaiton/models.py

--- exomesite/visualization/models.py
+++ exomesite/visualization/models.py
@@ -20,3 +20,45 @@ class Gene(models.Model):
     # some genes have variants so use both names for distinguishing variants
     def __str__(self):
         return self.name2 + ' (' + self.name + ')'
+
+class Variants(models.Model):
+    chrom = models.CharField(max_length=200)
+    position = models.IntegerField()
+    rsid = models.CharField(max_length=200)
+    reference = models.CharField(max_length=200)
+    alternate = models.CharField(max_length=200)
+    consequence = models.CharField(max_length=200)
+    proteinconsequence = models.CharField(max_length=200)
+    transcriptconsequence = models.CharField(max_length=200)
+    filter = models.CharField(max_length=200)
+    annotation = models.CharField(max_length=200)
+    flags = models.CharField(max_length=200)
+    allelecount = models.IntegerField()
+    allelenumber = models.BigIntegerField()
+    numberofhomozygotes = models.IntegerField()
+    allelefrequency = models.FloatField()
+    allelecountafrican = models.IntegerField()
+    allelenumberafrican = models.IntegerField()
+    homozygotecountafrican = models.IntegerField()
+    allelecounteastasian = models.IntegerField()
+    allelenumbereastasian = models.IntegerField()
+    homozygotecounteastasian = models.IntegerField()
+    allelecounteuropean_non_finnish_field = models.IntegerField()
+    allelenumbereuropean_non_finnish_field = models.IntegerField()
+    homozygotecounteuropean_non_finnish_field = models.IntegerField()
+    allelecountfinnish = models.IntegerField()
+    allelenumberfinnish = models.IntegerField()
+    homozygotecountfinnish = models.IntegerField()
+    allelecountlatino = models.IntegerField()
+    allelenumberlatino = models.IntegerField()
+    homozygotecountlatino = models.IntegerField()
+    allelecountother = models.IntegerField()
+    allelenumberother = models.IntegerField()
+    homozygotecountother = models.IntegerField()
+    allelecountsouthasian = models.IntegerField()
+    allelenumbersouthasian = models.IntegerField()
+    homozygotecountsouthasian = models.IntegerField()
+
+    def __str__(self):
+        return ("chrom; " + self.chrom + ", position: " + self.position +
+            ", reference/alternate: " + self.reference + '/' + self.alternate)
