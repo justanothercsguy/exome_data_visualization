7543ba3a0214c84eace2298a2b9f277322b576de
justanothercsguy <salsazishi@gmail.com>
2018-11-11 14:05:05 -0500

    Added function to limit the length of non coding parts of the first exon and last exon based on input variable nonCodingLimitLength
    Modified static/visualization/js/gene.js to contain function limitNonCodingExonLength(), which limits the length of the non-coding part of the first exon and last exon to nonCodingLimitLength, which is currently set to 200. So if cdsStart = 5000 and exonStarts[0].start = 4000, then the non-coding part of the first exon is from the base pair at position 4000 to the base pair at position 5000. This length is 5000 - 4000 = 1000, which exceeds the limitNonCodingExonLength, so exonStarts[0].start is set to 4800. Now 5000 - 4800 = 200, which is within the limit of limitNonCodingExonLength.

--- exomesite/visualization/static/visualization/js/gene.js
+++ exomesite/visualization/static/visualization/js/gene.js
@@ -1,17 +1,61 @@
 class Gene {
-    constructor(cdsStart, cdsEnd, exonStarts, exonEnds, exonCount) {
+    // NOTE: danger in this design because intializeExons depends
+    // on exonCount input coming before exonStarts, exonEnds
+    constructor(cdsStart, cdsEnd, exonCount, exonStarts, exonEnds) {
         this.cdsStart = cdsStart;
         this.cdsEnd = cdsEnd;
-        this.exons = this.initializeExons(exonStarts, exonEnds, exonCount);
         this.exonCount = exonCount;
+        this.exons = this.initializeExons(exonStarts, exonEnds, exonCount);
     }
 
     initializeExons(exonStarts, exonEnds, exonCount) {
         var exons = [];    
-        for (let i = 0; i < exonCount; i++) {
+        for (var i = 0; i < exonCount; i++) {
             exons.push(new Exon(exonStarts[i], exonEnds[i]));
         }
         return exons;
+    }
+
+    // NOTE: excluding functions to remove exons before cdsStart and after cdsEnd
+    // because such entries are extremely unlikely to occur - will add them in 
+    // later once the other data processing on exons are done
+
+
+    doesExonContainCdsStart(exon, cdsStart) {
+        // Don't split in the case where exonStart >= cdsStart and everything is coding,
+        // or the case where exonEnd < cdsStart and everything is non-coding
+        return (exon.start < cdsStart && exon.end >= cdsStart);
+    }
+      
+      
+    doesExonContainCdsEnd(exon, cdsEnd) {
+        // Don't split in the case where exonEnds[length - 1] <= cdsEnd and everything is coding,
+        // or the case where exonStarts[length - 1] > cdsEnd and everything is non-coding 
+        return (exon.start <= cdsEnd && exon.end > cdsEnd);
+    }
+
+    // Let i be the index of the exon that contains cdsStart within its domain
+    // Let j be the index of the exon that contains cdsEnd within its domain
+    // If (cdsStart - exon[i].start) > nonCodingLengthLimit, then we need to change exon[i].start
+    // so that (cdsStart - exon[i].start) = 200. Same thing if (exon[j].end - cdsEnd) > 200.
+    // Here, (cdsStart - exon[i].start) and (exon[j].end - cdsEnd) are the lengths of the non-coding
+    // exon partitions.
+    limitNonCodingExonLength(nonCodingLengthLimit) {
+        var firstExon = this.exons[0];
+        var lastExon = this.exons[this.exonCount - 1];
+        var cdsStart = this.cdsStart;
+        var cdsEnd = this.cdsEnd;
+
+        if (this.doesExonContainCdsStart(firstExon, cdsStart)
+        && (cdsStart - firstExon.start > nonCodingLengthLimit)) {
+            this.exons[0] = new Exon(cdsStart - nonCodingLengthLimit, firstExon.end);
+        }
+
+        if (this.doesExonContainCdsEnd(lastExon, cdsEnd)
+        && (lastExon.end - cdsEnd > nonCodingLengthLimit)) {
+            this.exons[this.exonCount - 1] = 
+                new Exon(lastExon.start, cdsEnd + nonCodingLengthLimit);
+        }
     }
 }
 
@@ -25,5 +69,3 @@ class Exon {
         return this.end - this.start;
     }
 }
-  
-  
