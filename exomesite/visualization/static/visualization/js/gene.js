88c6d9e12b17ca218d1ee4665561288af62d8a6f
justanothercsguy <salsazishi@gmail.com>
2018-11-11 10:16:31 -0500

    Added Gene class in static/visualization/js/gene.js
    Gene class takes in cdsStart, cdsEnd, exonStarts, exonEnds and will process these this data to fit into the chart accordingly.

---
+++ exomesite/visualization/static/visualization/js/gene.js
@@ -0,0 +1,8 @@
+class Gene {
+    constructor(cdsStart, cdsEnd, exonStarts, exonEnds) {
+        this.cdsStart = cdsStart;
+        this.cdsEnd = cdsEnd;
+        this.exonStarts = exonStarts;
+        this.exonEnds = exonEnds;
+    }
+}
