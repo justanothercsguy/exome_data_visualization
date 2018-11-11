5d3121f463acfad4dc333e261732c817fbeafc88
justanothercsguy <salsazishi@gmail.com>
2018-11-11 13:09:53 -0500

    Added Exon class and function for processing exonStarts and exonEnds array
    Modified static/visualization/js/gene.js to include an Exon class, and a intializeExons() function in the Gene class that takes two arrays, exonStarts and exonEnds, as input and returns a list of Exon objects.

--- exomesite/visualization/static/visualization/js/gene.js
+++ exomesite/visualization/static/visualization/js/gene.js
@@ -2,7 +2,29 @@ class Gene {
     constructor(cdsStart, cdsEnd, exonStarts, exonEnds) {
         this.cdsStart = cdsStart;
         this.cdsEnd = cdsEnd;
-        this.exonStarts = exonStarts;
-        this.exonEnds = exonEnds;
+        this.exons = this.initializeExons(exonStarts, exonEnds);
+    }
+
+    initializeExons(exonStarts, exonEnds) {
+        var exons = [];
+        var length = exonStarts.length;
+    
+        for (let i = 0; i < length; i++) {
+            exons.push(new Exon(exonStarts[i], exonEnds[i]));
+        }
+        return exons;
     }
 }
+
+class Exon {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    getLength() {
+        return this.end - this.start;
+    }
+}
+  
+  
