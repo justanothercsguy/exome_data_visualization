e073afcf1e1ab5f7c643c1c09e4be4257b48e47c
justanothercsguy <salsazishi@gmail.com>
2018-11-11 09:38:54 -0500

    Added function for initializing an svg object
    Added function addSvgToDiv() to static/visualization/js/chart.js, which takes the chart margins, dimensions (svg object dimensions minus the margins), and the name of the div to append the svg object to.

--- exomesite/visualization/static/visualization/js/chart.js
+++ exomesite/visualization/static/visualization/js/chart.js
@@ -1,9 +1,16 @@
 function addTooltip() {
     var tooltip = d3.select("body").append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0)
-      .style("font-size", 10);
-  
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+        .style("font-size", 10);
+
     return tooltip;
-  }
-  
+}
+
+function addSvgToDiv(margin, dimension, divName) {
+    var svgChartHandle = d3.select(divName).append("svg")
+        .attr("width", dimension.width + margin.left + margin.right)
+        .attr("height", dimension.height + margin.top + margin.bottom);
+
+    return svgChartHandle;
+}
